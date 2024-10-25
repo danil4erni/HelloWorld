7d8b7582ef9d4d035773dcda92f6a733da2be617
nakir <danil4erni@gmail.com>
2024-10-25 20:14:18 +0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        for (int i=0;i<100;i++){
+            System.out.println(i);
+        }
+    }
+}
